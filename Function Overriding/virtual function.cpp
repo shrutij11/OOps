46f07eb94c3b6fc3c200d2db97af00bdcf686164
Shruti Jaiswal <81301875+shrutij11@users.noreply.github.com>
2022-08-14 19:46:47 +0530

    Create virtual function.cpp

---
+++ Function Overriding/virtual function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+ class Base{
+     public:
+   void display(){
+         cout<<"Display of base"<<endl;
+     }
+     };
+     class Derived: public Base{
+     public:
+    void display(){
+         cout<<"Display of derived"<<endl;
+     }
+ };
+int main() {
+    // Write C++ code here
+Derived d;
+Base *p=&d;
+p->display();
+    return 0;
+}
+// Output: Display of base 
+
+// Using virtual Function
+#include <iostream>
+using namespace std;
+ class Base{
+     public:
+ virtual void display(){
+         cout<<"Display of base"<<endl;
+     }
+     };
+     class Derived: public Base{
+     public:
+    void display(){
+         cout<<"Display of derived"<<endl;
+     }
+ };
+int main() {
+    // Write C++ code here
+Derived d;
+Base *p=&d;
+p->display();
+    return 0;
+}
+//Output : Display of derived
